a6f5d2aa8b71723d6f449dcc7c869602cdfdf596
tjdqja1570 <tjdqja1570@gmail.com>
2022-07-12 15:55:08 +0900

    designer_createForm

--- src/main/java/com/study/designer/DesignerController.java
+++ src/main/java/com/study/designer/DesignerController.java
@@ -1,8 +1,14 @@
 package com.study.designer;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DesignerController {
-
+  
+  
+  @GetMapping("/create")
+  public String create() {
+    return "/create";
+  }
 }
