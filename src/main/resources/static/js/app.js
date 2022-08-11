f762c302997b047825ae434b8ad2740956ea02fa
tjdqja1570 <tjdqja1570@gmail.com>
2022-08-11 19:29:22 +0900

    websocket update

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -14,7 +14,7 @@ function setConnected(connected) {
 }
 
 function connect() {
-    var socket = new SockJS('/ws');
+    var socket = new SockJS('/wss');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         setConnected(true);
